2ad66b7440b2afb2b6ef2392cb148b87b4529b15
sophi <sophi@DESKTOP-4T3U5GI>
2020-10-14 12:10:26 +0200

    couleurs

--- AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
+++ AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
@@ -20,11 +20,11 @@ public class CommandTest implements CommandExecutor {
 		if(cmd.getName().equalsIgnoreCase("impostor") && sender instanceof Player) {
 			Player player = (Player)sender;
 			String name = player.getName();
-			player.sendMessage("&4Impostor " + name + ", get your weapon !");
+			player.sendMessage("§4Impostor " + name + ", get your weapon !");
 			ItemStack impostorSword = new ItemStack(Material.IRON_SWORD);
 			ItemMeta impostorSwordM = impostorSword.getItemMeta();
 			impostorSwordM.setUnbreakable(true);
-			impostorSwordM.setDisplayName("&4KILL");
+			impostorSwordM.setDisplayName("§4KILL");
 			impostorSwordM.addEnchant(Enchantment.DAMAGE_ALL, 200, true);
 			impostorSwordM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 			impostorSword.setItemMeta(impostorSwordM);
