426301a3db541a36f2216451de8471166fb0e939
sophi <sophi@DESKTOP-4T3U5GI>
2020-10-14 13:36:07 +0200

    ajout des objets imposteurs

--- AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
+++ AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
@@ -22,20 +22,60 @@ public class CommandTest implements CommandExecutor {
 			Player player = (Player)sender;
 			String name = player.getName();
 			player.sendMessage("§4Impostor " + name + ", get your weapon !"); //Changer pour la couleur
-			//Création de l'épée
-			ItemStack impostorSword = new ItemStack(Material.IRON_SWORD);
-			//Création des attributs de l'épée
-			ItemMeta impostorSwordM = impostorSword.getItemMeta();
-			impostorSwordM.setUnbreakable(true);
-			impostorSwordM.setDisplayName("§4KILL");
-			impostorSwordM.addEnchant(Enchantment.DAMAGE_ALL, 200, true);
-			impostorSwordM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-			impostorSwordM.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
-			//Ajout des attributs à l'épée
-			impostorSword.setItemMeta(impostorSwordM);
-			//Clear et ajoute l'épée
+			
+			//Création du kill
+			ItemStack impSword = new ItemStack(Material.IRON_SWORD);
+			//Création du sabotage des communications
+			ItemStack impComm = new ItemStack(Material.REDSTONE_BLOCK);
+			//Création du sabotage du réacteur
+			ItemStack impReact = new ItemStack(Material.TNT);
+			//Création du sabotage des lights
+			ItemStack impLight = new ItemStack(Material.REDSTONE_TORCH);
+			//Création du sabotage des portes
+			ItemStack impDoor1 = new ItemStack(Material.IRON_DOOR);
+			ItemStack impDoor2 = new ItemStack(Material.OAK_DOOR);
+			ItemStack impDoor3 = new ItemStack(Material.SPRUCE_DOOR);
+			ItemStack impDoor4 = new ItemStack(Material.JUNGLE_DOOR);
+			//Création du réport
+			ItemStack report = new ItemStack(Material.REDSTONE_BLOCK);
+			
+			//Création des attributs
+			ItemMeta impSwordM = impSword.getItemMeta();
+			ItemMeta impCommM = impComm.getItemMeta();
+			ItemMeta impReactM = impReact.getItemMeta();
+			ItemMeta impLightM = impLight.getItemMeta();
+			ItemMeta impDoor1M = impDoor1.getItemMeta();
+			ItemMeta impDoor2M = impDoor2.getItemMeta();
+			ItemMeta impDoor3M = impDoor3.getItemMeta();
+			ItemMeta impDoor4M = impDoor4.getItemMeta();
+			ItemMeta reportM = report.getItemMeta();
+			
+			impSwordM.setUnbreakable(true);
+			impSwordM.setDisplayName("§4KILL");
+			impSwordM.addEnchant(Enchantment.DAMAGE_ALL, 200, true);
+			impSwordM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+			impSwordM.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+			//Ajout des attributs
+			impSword.setItemMeta(impSwordM);
+			impComm.setItemMeta(impCommM);
+			impReact.setItemMeta(impReactM);
+			impLight.setItemMeta(impLightM);
+			impDoor1.setItemMeta(impDoor1M);
+			impDoor2.setItemMeta(impDoor2M);
+			impDoor3.setItemMeta(impDoor3M);
+			impDoor4.setItemMeta(impDoor4M);
+			report.setItemMeta(reportM);
+			//Clear et ajout des objets en main
 			player.getInventory().clear();
-			player.getInventory().addItem(impostorSword);
+			player.getInventory().addItem(impSword);
+			player.getInventory().addItem(impComm);
+			player.getInventory().addItem(impReact);
+			player.getInventory().addItem(impLight);
+			player.getInventory().addItem(impDoor1);
+			player.getInventory().addItem(impDoor2);
+			player.getInventory().addItem(impDoor3);
+			player.getInventory().addItem(impDoor4);
+			player.getInventory().addItem(report);
 			player.updateInventory();
 			return true;
 		}
