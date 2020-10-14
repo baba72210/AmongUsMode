8b798d946b76ab24cb79485263bb518cddbc234f
sophi <sophi@DESKTOP-4T3U5GI>
2020-10-14 12:58:38 +0200

    ajout commentaire impostor

--- AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
+++ AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
@@ -18,16 +18,22 @@ public class CommandTest implements CommandExecutor {
 
 		//épée impostor ONE SHOT
 		if(cmd.getName().equalsIgnoreCase("impostor") && sender instanceof Player) {
+			//On rend le sender en player pour mieux l'utiliser
 			Player player = (Player)sender;
 			String name = player.getName();
-			player.sendMessage("§4Impostor " + name + ", get your weapon !");
+			player.sendMessage("§4Impostor " + name + ", get your weapon !"); //Changer pour la couleur
+			//Création de l'épée
 			ItemStack impostorSword = new ItemStack(Material.IRON_SWORD);
+			//Création des attributs de l'épée
 			ItemMeta impostorSwordM = impostorSword.getItemMeta();
 			impostorSwordM.setUnbreakable(true);
 			impostorSwordM.setDisplayName("§4KILL");
 			impostorSwordM.addEnchant(Enchantment.DAMAGE_ALL, 200, true);
 			impostorSwordM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+			impostorSwordM.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+			//Ajout des attributs à l'épée
 			impostorSword.setItemMeta(impostorSwordM);
+			//Clear et ajoute l'épée
 			player.getInventory().clear();
 			player.getInventory().addItem(impostorSword);
 			player.updateInventory();
