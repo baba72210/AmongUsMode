cad66c66739ea298e339384e387fb175338a1025
sophi <sophi@DESKTOP-4T3U5GI>
2020-10-14 10:34:04 +0200

    Messages lancement/fin plugin

--- AmongUsMode/src/fr/charly/amongusmode/Main.java
+++ AmongUsMode/src/fr/charly/amongusmode/Main.java
@@ -3,5 +3,18 @@ package fr.charly.amongusmode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	/*
+	 * Messages au lancement et fin du plugin
+	 */
+	@Override
+	public void onEnable() {
+		System.out.println("Plug ON");
+	}
+	@Override
+	public void onDisable() {
+		System.out.println("Plug OFF");
+	}
+	
+	
 }
