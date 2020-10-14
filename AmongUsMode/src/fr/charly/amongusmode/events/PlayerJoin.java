5d8f0c618ee726e555b9606001ad4fc8073ad043
Cch4rly <charlyreux@orange.fr>
2020-10-14 13:36:03 +0200

    kick du tableau onlinePlayer quand le joueur leave le serv

--- AmongUsMode/src/fr/charly/amongusmode/events/PlayerJoin.java
+++ AmongUsMode/src/fr/charly/amongusmode/events/PlayerJoin.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import fr.charly.amongusmode.commands.CommandTest;
 
@@ -17,6 +18,12 @@ public class PlayerJoin implements Listener {
 		
 		CommandTest.onlinePlayers.add(p);
 	}	
+	@EventHandler
+	public void onLeave(PlayerQuitEvent e) {
+		Player p = (Player) e.getPlayer();
+		
+		CommandTest.onlinePlayers.remove(p);
+	}
 	
 		
 	}
