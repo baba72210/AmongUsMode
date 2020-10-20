5bc8fe9508e3fa8e46ed2cc41379e09850f9f783
Cch4rly <charlyreux@orange.fr>
2020-10-20 11:36:28 +0200

    ajout de ficher perso de s commandes

--- AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
+++ AmongUsMode/src/fr/charly/amongusmode/commands/CommandTest.java
@@ -119,51 +119,6 @@ public class CommandTest implements CommandExecutor {
 			}
 
 
-			if(cmd.getName().equalsIgnoreCase("teamSet")) {
-				//ajout dans les crewmates de tout les joueurs en ligne ayant le tag "amongUs"
-				for(Player p : onlinePlayers) {
-					if (p.getScoreboardTags().contains("amongUs")) {
-						crewMates.add(p);
-					}
-				}
-				//deplacement de deux players de la team crewmate a la team imposteur
-				int numImpostor = (int)(Math.random() * (crewMates.size()+1));
-				impostors.add(crewMates.get(numImpostor));
-				crewMates.remove(numImpostor);
-				numImpostor = (int)(Math.random() * (crewMates.size() + 1));
-				impostors.add(crewMates.get(numImpostor));
-				crewMates.remove(numImpostor);
-			}
-
-
-			//commande de test team
-			if(cmd.getName().equalsIgnoreCase("teamcheck")) {
-				player.sendMessage("OnlinePlayers:");
-				for(Player p : onlinePlayers) {
-					player.sendMessage("-"+p.getName());
-				}
-				player.sendMessage("impostors:");
-				for(Player p : impostors) {
-					player.sendMessage("-"+p.getName());
-				}
-				player.sendMessage("crewmates:");
-				for(Player p : crewMates) {
-					player.sendMessage("-"+p.getName());
-				}
-				//if(onlinePlayers.contains(player))player.sendMessage();
-				return true;
-			}
-			
-			if(cmd.getName().equalsIgnoreCase("vote")) {
-				double rad=-11* Math.PI/12;
-				for(Player p : onlinePlayers) {
-					if (p.getScoreboardTags().contains("amongUs")) {
-						p.teleport(new Location(p.getWorld(), 4*Math.sin(rad), 4*Math.cos(rad), 10));
-						rad+=2*Math.PI/12;
-					}
-				}
-				
-			}
 
 
 			if(cmd.getName().equalsIgnoreCase("test")) {
